654b5869dea14dd8564bd989707436f2d6063d53
Saleh <xsaleh2090@gmail.com>
2023-11-11 19:25:35 +0300

    finishing the migration lecture

--- database/migrations/2023_11_11_143958_create_products_table.php
+++ database/migrations/2023_11_11_143958_create_products_table.php
@@ -11,9 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
+            $table->decimal('price', 12, 2)->nullable();
+            $table->longText('description')->nullable();
+            $table->foreignId('category_id')->constrained('categories', 'id')->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -23,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('products');
     }
 };
--- database/migrations/2023_11_11_152936_add_description_column_to_categories_table.php
+++ database/migrations/2023_11_11_152936_add_description_column_to_categories_table.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->timestamps();
+        Schema::table('categories', function (Blueprint $table) {
+            $table->longText('description')->after('name')->nullable();
         });
     }
 
@@ -23,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropColumn('description');
+        });
     }
 };
